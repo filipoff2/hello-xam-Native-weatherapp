001b876935367678ee3e6a349e864ba962e9dfb5
Bogusław Błoński <bblonski@o2.pl>
2020-03-30 14:48:58 +0200

    Update ApiConstants.cs

--- xamApp/WeatherApp/Common/ApiConstants.cs
+++ xamApp/WeatherApp/Common/ApiConstants.cs
@@ -14,9 +14,8 @@ namespace WeatherApp.Common
         public const string BasePath = "https://api.openweathermap.org/data/2.5/";
         public const string iconPath = "https://openweathermap.org/img/w/";
 
-        public const string ApiKey = "f5dd1350e64f758199c1cca5f98b2ed8";
-        //private const string ApiKey = "9322eba14d4665ad66b06b489df48ecb";
-
+        public const string ApiKey = "xxx_your_code";
+        
         public const int Days = 14;
         public const int WeatherHours= 6;
 
